1da43fdcd81fc6bc82ca0f89bef7cfbd1241f3d4
DYBEDY <rd.iosdeveloper@gmail.com>
2022-02-19 20:37:49 +0300

    Initial Commit

---
+++ rgbSliders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rgbSliders
+//
+//  Created by Roman on 19.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rgbSliders/rgbSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  rgbSlidersApp.swift
+//  rgbSliders
+//
+//  Created by Roman on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct rgbSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
